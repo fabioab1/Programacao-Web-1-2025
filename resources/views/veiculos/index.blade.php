7a5a7a80cd33a60a573474894cf24a8b8b9e5a36
Luiz Felipe <luiz.bore2004@gmail.com>
2025-04-07 20:08:22 -0300

    GerenciarVeículos
    CRUD da função fundamental GerenciarVeiculos

---
+++ resources/views/veiculos/index.blade.php
@@ -0,0 +1,59 @@
+@extends('layout')
+
+@section('conteudo')
+
+<h1>Gerenciar Veículos</h1>
+
+<a class="btn btn-primary mb-3" href="/veiculos/create">Novo Veículo</a>
+
+@if (session('erro'))
+<div class="alert alert-danger">
+	{{ session('erro') }}
+</div>
+@endif
+
+@if (session('sucesso'))
+<div class="alert alert-success">
+	{{ session('sucesso') }}
+</div>
+@endif
+
+<h2>Registro de Veículos</h2>
+
+<table class="table table-hover table-striped">
+	<thead>
+		<tr>
+			<th>ID</th>
+			<th>Placa</th>
+			<th>Nome</th>
+			<th>Modelo</th>
+			<th>Marca</th>
+			<th>Ano</th>
+			<th>Tipo</th>
+			<th>Cor</th>
+			<th>Capacidade</th>
+			<th>Situação</th>
+		</tr>
+	</thead>
+	<tbody>
+		@foreach ($veiculos as $veiculo)
+		<tr>
+			<td>{{ $veiculo->id }}</td>
+			<td>{{ $veiculo->placa }}</td>
+			<td>{{ $veiculo->nome }}</td>
+			<td>{{ $veiculo->modelo }}</td>
+			<td>{{ $veiculo->marca }}</td>
+			<td>{{ $veiculo->ano }}</td>
+			<td>{{ $veiculo->tipo }}</td>
+			<td>{{ $veiculo->cor }}</td>
+			<td>{{ $veiculo->capacidade }}</td>
+			<td>{{ $veiculo->situacao }}</td>
+			<td>
+				<a href="/veiculos/{{ $veiculo->id }}/edit" class="btn btn-warning">Editar</a>
+				<a href="/veiculos/{{ $veiculo->id }}" class="btn btn-info">Consultar</a>
+			</td>
+		</tr>
+		@endforeach
+	</tbody>
+</table>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\VeiculoController;
+use App\Models\Veiculo;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('inicio');
 });
+
+route::resource("veiculos", VeiculoController::class);
