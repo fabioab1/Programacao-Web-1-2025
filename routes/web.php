f289c1b4ef70919b6da2812ec1b264203ebfff65
Luiz Felipe <luiz.bore2004@gmail.com>
2025-04-07 21:07:23 -0300

    GerenciarCidades

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CidadeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('inicio');
 });
+Route::resource('cidades', CidadeController::class);
