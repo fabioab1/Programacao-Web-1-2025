24494f4ecb723a1cc859ab5048b9e9458f399369
fabioab1 <fabiobonanatodeabreu990@gmail.com>
2026-02-17 19:30:11 -0300

    fix: corrige a responsividade do include cabecalho

--- resources/views/cabecalho.blade.php
+++ resources/views/cabecalho.blade.php
@@ -1,22 +1,43 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm">
     <div class="container-fluid">
+
+        {{-- Logo --}}
         <a class="navbar-brand fw-bold text-light" href="/">e-PAS</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Alternar navegação">
+
+        {{-- Botão Mobile --}}
+        <button class="navbar-toggler" type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Alternar navegação">
             <span class="navbar-toggler-icon"></span>
         </button>
 
+        {{-- Menu --}}
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+            {{-- Links principais --}}
+            <ul class="navbar-nav me-auto mb-3 mb-lg-0 gap-lg-2">
+
+                {{-- ADMIN --}}
                 @if (Auth::user()->role == 'ADM')
+
                     <li class="nav-item">
                         <a class="nav-link text-light" href="/inicio">Início</a>
                     </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+
+                    {{-- Dropdown Usuários --}}
+                    <li class="nav-item dropdown position-static position-lg-relative">
+                        <a class="nav-link dropdown-toggle text-light"
+                           href="#"
+                           role="button"
+                           data-bs-toggle="dropdown"
+                           aria-expanded="false">
                             Usuários
                         </a>
-                        <ul class="dropdown-menu">
+
+                        <ul class="dropdown-menu dropdown-menu-lg-start dropdown-menu-end shadow">
                             <li><a class="dropdown-item" href="/cargos">Cargos</a></li>
                             <li><hr class="dropdown-divider"></li>
                             <li><a class="dropdown-item" href="/pacientes">Pacientes</a></li>
@@ -24,11 +45,18 @@
                             <li><a class="dropdown-item" href="/administradores">Administradores</a></li>
                         </ul>
                     </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+
+                    {{-- Dropdown Transporte --}}
+                    <li class="nav-item dropdown position-static position-lg-relative">
+                        <a class="nav-link dropdown-toggle text-light"
+                           href="#"
+                           role="button"
+                           data-bs-toggle="dropdown"
+                           aria-expanded="false">
                             Transporte
                         </a>
-                        <ul class="dropdown-menu">
+
+                        <ul class="dropdown-menu dropdown-menu-lg-start dropdown-menu-end shadow">
                             <li><a class="dropdown-item" href="/pontos">Pontos</a></li>
                             <li><a class="dropdown-item" href="/cidades">Cidades</a></li>
                             <li><a class="dropdown-item" href="/veiculos">Veículos</a></li>
@@ -37,58 +65,81 @@
                             <li><a class="dropdown-item" href="/viagens">Viagens</a></li>
                         </ul>
                     </li>
+
                 @endif
 
+                {{-- MOTORISTA --}}
                 @if (Auth::user()->role == 'MOT')
                     <li class="nav-item">
                         <a class="nav-link text-light" href="/inicio-mot">Início</a>
                     </li>
                 @endif
 
+                {{-- PACIENTE --}}
                 @if (Auth::user()->role == 'PAC')
+
                     <li class="nav-item">
                         <a class="nav-link text-light" href="/inicio-pac">Início</a>
                     </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+
+                    <li class="nav-item dropdown position-static position-lg-relative">
+                        <a class="nav-link dropdown-toggle text-light"
+                           href="#"
+                           role="button"
+                           data-bs-toggle="dropdown"
+                           aria-expanded="false">
                             Transporte
                         </a>
-                        <ul class="dropdown-menu">
+
+                        <ul class="dropdown-menu dropdown-menu-end shadow">
                             <li><a class="dropdown-item" href="/solicitacoes">Solicitações</a></li>
                         </ul>
                     </li>
+
                 @endif
+
             </ul>
 
-            {{-- Perfil --}}
-            <div class="d-flex align-items-center gap-2">
-                @if (Auth::user()->role == 'ADM')
-                    <div class="dropdown">
-                        <button class="btn btn-outline-light dropdown-toggle" type="button" data-bs-toggle="dropdown">
-                            Meu perfil
-                        </button>
-                        <ul class="dropdown-menu dropdown-menu-end">
-                            <li><a class="dropdown-item" href="/alterar-dados/admin/{{ Auth::user()->id }}">Alterar dados</a></li>
-                        </ul>
-                    </div>
-                @endif
+            {{-- Perfil + Logout --}}
+            <div class="d-flex flex-column flex-lg-row align-items-lg-center gap-2">
 
-                @if (Auth::user()->role == 'PAC')
-                    <div class="dropdown">
-                        <button class="btn btn-outline-light dropdown-toggle" type="button" data-bs-toggle="dropdown">
-                            Meu perfil
-                        </button>
-                        <ul class="dropdown-menu dropdown-menu-end">
-                            <li><a class="dropdown-item" href="/alterar-dados/paciente/{{ Auth::user()->id }}">Alterar dados</a></li>
-                        </ul>
-                    </div>
-                @endif
+                {{-- Perfil --}}
+                <div class="dropdown">
+                    <button class="btn btn-outline-light dropdown-toggle w-100 w-lg-auto"
+                            type="button"
+                            data-bs-toggle="dropdown">
+                        Meu perfil
+                    </button>
 
-                {{-- Sair --}}
-                <form method="POST" action="/logout">
+                    <ul class="dropdown-menu dropdown-menu-end shadow">
+                        @if (Auth::user()->role == 'ADM')
+                            <li>
+                                <a class="dropdown-item"
+                                   href="/alterar-dados/admin/{{ Auth::user()->id }}">
+                                    Alterar dados
+                                </a>
+                            </li>
+                        @endif
+
+                        @if (Auth::user()->role == 'PAC')
+                            <li>
+                                <a class="dropdown-item"
+                                   href="/alterar-dados/paciente/{{ Auth::user()->id }}">
+                                    Alterar dados
+                                </a>
+                            </li>
+                        @endif
+                    </ul>
+                </div>
+
+                {{-- Logout --}}
+                <form method="POST" action="/logout" class="w-100 w-lg-auto">
                     @csrf
-                    <button class="btn btn-danger" type="submit">Sair</button>
+                    <button class="btn btn-danger w-100">
+                        Sair
+                    </button>
                 </form>
+
             </div>
 
         </div>
